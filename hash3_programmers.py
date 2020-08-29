88084b91effa52ce3693af828525a038f287a50d
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2020-08-29 14:44:51 +0900

    Create hash3_programmers.py

---
+++ hash3_programmers.py
@@ -0,0 +1,11 @@
+#위장 문제
+
+def solution(clothes):
+    from collections import Counter
+    from functools import reduce
+    cnt = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
+    
+# reduce 함수? 흠.. 람다식도 한번 더 공부해보기
+# 이건 수학문제... 휴우..
