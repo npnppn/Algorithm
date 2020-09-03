466bd0bf927dbb140e796079fbcf0637af0d7829
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2020-09-03 23:48:50 +0900

    Create stack1_leetcode.py

---
+++ stack1_leetcode.py
@@ -0,0 +1,18 @@
+#20번 #valid-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')':'(',
+            '}':'{',
+            ']':'[',
+        }
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] != stack.pop():
+                return False
+        return len(stack) == 0
+        
+  # 스택 가장 기본적인 문제.
