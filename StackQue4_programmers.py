7387fb8d88c3873220c6efac40b304e50892ea18
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2020-09-09 18:06:34 +0900

    Create StackQue4_programmers.py

---
+++ StackQue4_programmers.py
@@ -0,0 +1,19 @@
+#프로그래머스 스택큐 4번문제
+
+def solution(priorities, location):
+    answer = 0
+
+    sample = [0] * len(priorities)
+    sample[location] = 1
+
+    while max(sample) == 1:
+        if priorities[0] == max(priorities):
+            priorities.pop(0)
+            sample.pop(0)
+            answer += 1
+
+        else:
+            priorities.append(priorities.pop(0))
+            sample.append(sample.pop(0))
+
+    return answer
