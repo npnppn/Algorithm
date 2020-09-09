d9ce6dfb9ae1b6fc120f76077c97dbef98d2ac95
ParkHyungMin <61404972+npnppn@users.noreply.github.com>
2020-09-09 14:54:53 +0900

    Create StackQue1_programmers.py

---
+++ StackQue1_programmers.py
@@ -0,0 +1,33 @@
+#프로그래머스 스택큐 주식문제
+
+from collections import deque
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
+    
+    
+    
+ #2중포문으로 푼사람도 많이 보임. 참고해볼것
+ def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
